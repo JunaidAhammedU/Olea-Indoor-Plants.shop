2c67107d599e032492d5dbc65f89d14d0125bb58
junaid <junaidsecond758@gmail.com>
2023-08-05 10:22:46 +0530

    dbs connections

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnect = () => {
   try {
-    const connect = mongoose.connect("mongodb://127.0.0.1:27017/OleaDB");
+    const connect = mongoose.connect("mongodb://127.0.0.1:27017/oleaDBS");
     console.log("DB connected");
   } catch (error) {
     console.log("DB not connected");
